aa6b5fce7ec826ed25fbc0f128fe56ecd904e218
vamaral2020 <vagner2013.vrda@gmail.com>
2021-03-09 22:33:20 -0300

    Criando o Repositório no Git Hub

---
+++ src/main/java/br/com/digitalinnovation/heroisapi/HeroisapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.digitalinnovation.heroisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroisapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroisapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/digitalinnovation/heroisapi/HeroisapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digitalinnovation.heroisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroisapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
